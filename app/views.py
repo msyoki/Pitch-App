494daeea372a77bdd1fd3ad6ec611656b36909fa
Msyoki <msyokimutua@gmail.com>
2020-09-22 23:36:00 +0300

    set routes for added templates

--- app/views.py
+++ app/views.py
@@ -12,3 +12,22 @@ def about():
     title= 'About - Pitch APP '
     return render_template('about.html',title=title)
 
+@app.route('/pickup_lines')
+def pickup_lines():
+    title= 'pickup_lines '
+    return render_template('pickup_lines.html',title=title)
+
+@app.route('/interview_pitch')
+def interview_pitch():
+    title= 'interview_pitch '
+    return render_template('interview_pitch.html',title=title)
+
+@app.route('/products_pitch')
+def products_pitch():
+    title= 'products_pitch '
+    return render_template('products_pitch.html',title=title)
+
+@app.route('/promotion_pitch')
+def promotion_pitch():
+    title= 'promotion_pitch '
+    return render_template('promotion_pitch.html',title=title)
