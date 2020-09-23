046809c3f62bab81c7fd4f2628b3c1b4ec059b2f
Msyoki <msyokimutua@gmail.com>
2020-09-23 13:06:11 +0300

    update navbar and install sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from .config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
@@ -7,13 +11,13 @@ app.config['SECRET_KEY'] = '3a58c9a4a8d5d9e456de6650505514d5'
 
 posts = [
     {
-        'title':'Blog post 1',
+        'title':'Pitch post 1',
         'author':'Erick Musyoki',
         'date_post':'22 Sep 2020',
         'content':'Fist post content'
     },
     {
-        'title':'Blog post 2',
+        'title':'Pitch post 2',
         'author': 'Mike Mutua',
         'date_post':'23 Sep 2020',
         'content':'Second post content'
@@ -22,8 +26,8 @@ posts = [
 
 
 
-
-
+# Initializing flask extensions
+db.init_app(app)
 
 
 # Setting up configuration
