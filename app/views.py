fd7577b97618f33b21c69a71339779cf29f64975
Msyoki <msyokimutua@gmail.com>
2020-09-23 09:00:22 +0300

    activate start.sh

--- app/views.py
+++ app/views.py
@@ -38,7 +38,7 @@ def promotion_pitch():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html',title='Register',form=form)
 
