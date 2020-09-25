5bb2599ac57a1cab95caa54df938a190b3b4e715
Msyoki <msyokimutua@gmail.com>
2020-09-25 18:50:17 +0300

    add forign keys to user model

--- app/models.py
+++ app/models.py
@@ -11,11 +11,19 @@ def load_user(user_id):
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
+    
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime,default=datetime.utcnow)
+
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
