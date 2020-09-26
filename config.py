c6d6e749400fc1ceafa651a2bea926bccf85f90b
msyoki <msyokimutua@gmail.com>
2020-09-26 22:35:12 +0300

    add welcome email config

--- config.py
+++ config.py
@@ -8,8 +8,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://msyoki:psql20*@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
     
 
 class ProdConfig(Config):
@@ -17,6 +21,15 @@ class ProdConfig(Config):
     Production configuration child class 
     '''
     pass
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG=True
 
 class DevConfig(Config):
     '''
@@ -30,5 +43,6 @@ class DevConfig(Config):
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
