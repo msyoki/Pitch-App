df0cb75c299ffdc0c998dc635be5ef10fc04db79
msyoki <msyokimutua@gmail.com>
2020-09-27 12:46:15 +0300

    update models

--- app/models.py
+++ app/models.py
@@ -40,16 +40,16 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Role(db.Model):
-    __tablename__ = 'roles'
+# class Role(db.Model):
+#     __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
+#     id = db.Column(db.Integer,primary_key = True)
+#     name = db.Column(db.String(255))
+#     users = db.relationship('User',backref = 'role',lazy="dynamic")
 
 
-    def __repr__(self):
-        return f'User {self.name}'
+#     def __repr__(self):
+#         return f'User {self.name}'
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
